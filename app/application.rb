7e906bc11257f1d175121750b53520be949b43a4
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-28 15:40:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    Time.now.morning = (1..12)
-    Time.now.afternoon = (13..24)
+    morning = (1..12)
+    afternoon = (13..24)
 
-    if Time.now == morning
+    if t.hour == morning
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
