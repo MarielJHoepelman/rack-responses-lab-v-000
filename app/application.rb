c9d7abf1a3a2ff2097fc3d16622483fe7ddec9d6
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-28 15:54:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if Time.now.hour > 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
